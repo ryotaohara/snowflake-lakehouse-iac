7024214ec2fd22790ceba0b1327585dc6d0376ba
ryotaohara <rytohr339@gmail.com>
2026-03-15 14:52:20 +0900

    Fixed main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    snowflake = {
+      source  = "snowflakedb/snowflake"
+      version = "~> 0.28.6" # Specify a valid version constraint
+    }
+  }
+}
+
 # テスト用データベースの作成
 resource "snowflake_database" "test" {
   name    = "DB_TEST_TERRAFORM"
