934296481d44500b767bc4390dfa1ceffe22599a
ryotaohara <rytohr339@gmail.com>
2026-03-20 22:56:31 +0900

    Updated Snowflake account info

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -45,9 +45,3 @@ variable "SNOWFLAKE_WORKLOAD_IDENTITY_PROVIDER" {
   type        = string
   sensitive   = true
 }
-
-variable "SNOWFLAKE_AUDIENCE" {
-  description = "Snowflake audience for OIDC"
-  type        = string
-  sensitive   = true
-}
